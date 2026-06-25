0b1da193f8b1c037d9cbfea8774460e849986c47
HAWA-nosan <korei-ryo@awi.co.jp>
2026-06-26 00:37:25 +0900

    Update app_rikuto.py

--- app_rikuto.py
+++ app_rikuto.py
@@ -133,6 +133,4 @@ def get_climate_data():
         return jsonify({"status": "error", "message": str(e), "trace": traceback.format_exc()}), 500
 
 if __name__ == "__main__":
-    import os
-    port = int(os.environ.get("PORT", 8080))
-    app.run(host="0.0.0.0", port=port)
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
