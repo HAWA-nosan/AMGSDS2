1b025a2f007642a671557381d540a77826c0dafd
HAWA-nosan <korei-ryo@awi.co.jp>
2026-06-26 02:08:08 +0900

    Update app_sweet-corn2.py

--- app_sweet-corn2.py
+++ app_sweet-corn2.py
@@ -267,4 +267,6 @@ def get_climate_data():
         return jsonify({"status": "error", "message": str(e), "trace": traceback.format_exc()}), 400
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8080)
+    import os
+    port = int(os.environ.get("PORT", 8080))
+    app.run(host="0.0.0.0", port=port)
